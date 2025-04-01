b4fc7acf45506ce24f7a292278350448e2d50102
alessiozaccato <alessiozaccato@gmail.com>
2025-04-01 19:59:44 +0200

    FIX: pages path

---
+++ src/pages/Accesories.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+function Accessories() {
+    const [accessories, setAccessories] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        fetch('http://localhost:3000/products/accessories')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Errore durante il recupero dei prodotti');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setAccessories(data);
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>Caricamento...</p>;
+    if (error) return <p>Errore: {error}</p>;
+
+    return (
+        <div>
+            <h1>Lista Prodotti</h1>
+            <ul>
+                {accessories.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image_url} alt={product.name} style={{ width: '50px', height: '50px' }} />
+                        {product.description} - {product.price}€
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Accessories;

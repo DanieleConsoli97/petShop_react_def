fc903393d46788b9b495e18ec80e3c410c129236
alessiozaccato <alessiozaccato@gmail.com>
2025-04-03 12:21:43 +0200

    FIX: cart

--- src/pages/Carrello.jsx
+++ src/pages/Carrello.jsx
@@ -13,24 +13,13 @@ function Carrello() {
         <h1 className='text-center'>Carrello</h1>
         <div className='row'>
           {carrello.map((product) => (
-            // <div key={prodotto.slug} className='col-md-4'>
-            //   <div className='card mb-4'>
-            //     <img src={prodotto.immagine} className='card-img-top' alt={prodotto.nome} />
-            //     <div className='card-body'>
-            //       <h5 className='card-title'>{prodotto.nome}</h5>
-            //       <p className='card-text'>Prezzo: {prodotto.prezzo} €</p>
-            //       <p className='card-text'>Quantità: {prodotto.quantity}</p>
-            //       <button onClick={() => rimuoviDalCarrello(prodotto.slug)} className='btn btn-danger'>Rimuovi dal carrello</button>
-            //     </div>
-            //   </div>
-            // </div>
-
-            <div className="card" key={product.id}>
+            <div className="card" key={product.slug}>
               <img src={product.image_url} className="card-img-top" alt={product.name} />
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.price} €</p>
-                <button onClick={() => rimuoviDalCarrello(prodotto.slug)} className='btn btn-danger'>Rimuovi dal carrello</button>
+                <p className="card-text">Prezzo: {product.price} €</p>
+                <p className="card-text">Quantità: {product.quantity}</p>
+                <button onClick={() => rimuoviDalCarrello(product.slug)} className='btn btn-danger'>Rimuovi dal carrello</button>
                 <Link to={`/prodotti/${product.slug}`} className="btn btn-primary">
                   Vedi Dettagli
                 </Link>

ebcb753c698994dd7427c001ee745bb3dccc0feb
alessiozaccato <alessiozaccato@gmail.com>
2025-04-07 10:30:35 +0200

    FIX: bracket on wishList

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -15,21 +15,20 @@ function WishList() {
                 ) : (
                     <div className='row'>
                         {wishList.map((product) => (
-           
-                           <div className="card" key={product.slug}>
-                            <img src={product.image_url} className="card-img-top" alt={product.name} />
-                            <div className="card-body">
-                                <h5 className="card-title">{product.name}</h5>
+                            <div className="card" key={product.slug}>
+                                <img src={product.image_url} className="card-img-top" alt={product.name} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.name}</h5>
 
-                                {/* Aggiunto d-flex per affiancare i bottoni */}
-                                <div className="d-flex gap-2">
-                                    <button onClick={() => rimuoviDallaWishList(product.slug)} className='btn btn-danger flex-grow-1'>
-                                        Rimuovi dalla WishList
-                                    </button>
-                                    <Link to={`/prodotti/${product.slug}`} className="btn btn-primary flex-grow-1">
-
-                                        Vedi Dettagli
-                                    </Link>
+                                    {/* Aggiunto d-flex per affiancare i bottoni */}
+                                    <div className="d-flex gap-2">
+                                        <button onClick={() => rimuoviDallaWishList(product.slug)} className='btn btn-danger flex-grow-1'>
+                                            Rimuovi dalla WishList
+                                        </button>
+                                        <Link to={`/prodotti/${product.slug}`} className="btn btn-primary flex-grow-1">
+                                            Vedi Dettagli
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         ))}

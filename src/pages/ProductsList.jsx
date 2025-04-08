70f40995491aaa5b7acccba2930097764d537a26
chiaralle <chiaraallegrino@gmail.com>
2025-04-08 09:28:41 +0200

    producstlist

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -29,85 +29,21 @@ function ProductList() {
             });
     }, []);
 
-    if (loading) return (
-
-        <div
-            className="container d-flex justify-content-center align-items-center"
-            style={{ height: '100vh' }}
-        >
-            <p>Caricamento...</p>
-        </div >
-    );
-
-    if (error) return <p>Errore: {error}</p>;
-
-<<<<<<< HEAD
-return (
-    <>
-         <h1 className='text-center my-4'>Lista Prodotti</h1>
-        <div className="container d-flex justify-content-end mb-3">
-            <button className={`btn btn-outline-success me-2 ${isGridView ? 'active' : ''}`} onClick={() => setIsGridView(true)}>
-                <BsFillGrid3X3GapFill />
-            </button>
-            <button className={`btn btn-outline-success ${!isGridView ? 'active' : ''}`} onClick={() => setIsGridView(false)}>
-                <GrSort />
-            </button>
-        </div>
-        {isGridView ? (
-            // Grid View
-            <div className="products-container grid-view">
-                {products.map(product => (
-                    <div className="products-card" key={product.id}>
-                        <img src={product.image_url} className="card-img-top" alt={product.name} />
-                        <div className="card-body">
-                            <h5 className="card-title">{product.name}</h5>
-                            <p className="card-text">{product.price} €</p>
-                            <Link to={`/prodotti/${product.slug}`} className="btn btn-success">
-                                Vedi Dettagli
-                            </Link>
-                        </div>
-                    </div>
-                ))}
+    if (loading) {
+        return (
+            <div
+                className="container d-flex justify-content-center align-items-center"
+                style={{ height: '100vh' }}
+            >
+                <p>Caricamento...</p>
             </div>
-        ) : (
-            // List View
-             <div className="container">
-        <div className="list-group list-group-flush">
-            {products.map((product) => (
-                <div className="list-group-item py-4" key={product.id}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img 
-                                src={product.image_url} 
-                                className="img-fluid p-3" 
-                                alt={product.name}
-                                style={{ objectFit: 'contain', height: '250px', width: '100%' }}
-                            />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="h-100 d-flex flex-column justify-content-between">
-                                <div>
-                                    <h4 className="mb-3">{product.name}</h4>
-                                    <p className="text-muted mb-3">{product.brand}</p>
-                                    <p className="mb-3">{product.description}</p>
-                                </div>
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <h5 className="mb-0 fw-bold">{product.price} €</h5>
-                                    <Link to={`/prodotti/${product.slug}`} className="btn btn-success">
-                                        Vedi Dettagli
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    </div>
-        )}
-    </>
-);
-=======
+        );
+    }
+    
+    if (error) {
+        return <p>Errore: {error}</p>;
+    }
+    
     return (
         <>
             <h1 className='text-center my-4'>Lista Prodotti</h1>
@@ -125,7 +61,9 @@ return (
                         <img src={product.image_url} className={`card-img-top ${isGridView ? '' : 'list-image'}`} alt={product.name} />
                         <div className={`card-body ${isGridView ? '' : 'list-body'}`}>
                             <h5 className="card-title">{product.name}</h5>
-                            {product.discounted_price !== null ? <p className="card-text">{product.discounted_price}€</p> : <p className="price">{product.price}€</p>}
+                            {product.discounted_price !== null
+                                ? <p className="card-text">{product.discounted_price}€</p>
+                                : <p className="price">{product.price}€</p>}
                             <Link to={`/prodotti/${product.slug}`} className="btn btn-primary">
                                 Vedi Dettagli
                             </Link>
@@ -135,7 +73,7 @@ return (
             </div>
         </>
     );
->>>>>>> 15c7d4b6f9c3bda42d1be9b9c8e4d12c7e4f46ec
+eee    
 }
 
 export default ProductList;
